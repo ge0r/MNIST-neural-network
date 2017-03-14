68b950f50257bb071c90811dc2a9263baa90ab38
George Gkioulis <george.gkioulis@gmail.com>
2017-03-14 11:49:55 +0100

    Fix file input output

--- main.py
+++ main.py
@@ -1,42 +1,31 @@
 import trainer
 import network
 import time
-import csv
 
 start = time.time()
 high_score = 0
-epochs = 5
+epochs = 10
 count = 0
 
-file1 = open('csvs/threeL.csv', "wb")
-writer1 = csv.writer(file1, delimiter=',')
+for h in [0.5]:
 
-file2 = open('csvs/fourL.csv', "wb")
-writer2 = csv.writer(file2, delimiter=',')
+    for minibatch_size in [1]:
 
-for h in [0.01, 0.05, 0.1, 0.3, 0.5, 0.8, 1.1, 5, 20]:
-
-    for minibatch_size in [3, 5, 10, 15, 20, 50, 100, 250, 1000]:
-
-        for layer1 in [1, 3, 5, 10, 15, 20, 50, 100, 250]:
+        for layer1 in [250]:
             t = trainer.TrainerTester(network.Network([784, layer1, 10]), minibatch_size, h, epochs)
-            score = t.train(writer1)
+            score = t.train()
             if score > high_score:
                 high_score = score
 
-            for layer2 in [1, 3, 5, 10, 15, 20, 50, 100, 250]:
+            for layer2 in [17]:
                 t2 = trainer.TrainerTester(network.Network([784, layer1, layer2, 10]), minibatch_size, h, epochs)
-                score = t2.train(writer2)
+                score = t2.train()
                 if score > high_score:
                     high_score = score
 
                 count += 1
                 print "completed "+str(count)+" trainings"
-
 end = time.time()
-
-file1.close()
-file2.close()
 
 print "hours elapsed: "+str((end - start)/3600)
 print "high_score: "+str(high_score)
